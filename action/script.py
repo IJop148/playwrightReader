a773ef318410aa00ddd87e0cde1fb10c09241cd1
Jop <jopteungores@gmail.com>
2025-09-22 15:05:23 +0200

    first commit

---
+++ action/script.py
@@ -0,0 +1,13 @@
+import httpx
+import sys
+import os
+
+server = sys.argv[1]
+file = sys.argv[2]
+ssl_verify = sys.argv[3]
+
+with httpx.Client(base_url=server, verify=bool(ssl_verify)) as c:
+    f = c.post(url=os.environ["GITHUB_SHA"], files=os.open(file))
+    json = f.json()
+    with open("GITHUB_OUTPUT", "a") as fh:
+        print(f"fileloc={server+json["file"]}", file=fh)
---
+++ server/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+import os
+import pathlib
+
+app = Flask(__name__)
+app.static_folder = "./static"
+
+@app.route("/<path:path>")
+def show(path):
+    new_path = pathlib.Path(app.static_folder, path)
+    if new_path.is_file():
+        return new_path.open("rb")
+    files = list(new_path.glob("*"))
+    return_str = ""
+    for i in files:
+        return_str += f"<a href='{str(i.resolve()).split("static")[-1]}'>{i.name}</a><br>"
+    return return_str
+
+@app.route("/<gitsha>", methods=["POST"])
+def upload(gitsha: str):
+    if(gitsha == None):
+        return {}, 204
+    
+    file = pathlib.Path(app.static_folder, gitsha)
+
+    files = list(file.glob("index_*.html"))
+    files.sort(reverse=True)
+    num = int(files[0].name.split("_")[-1].split(".html")[0])+1
+    
+    new_filename = pathlib.Path(file, f"index_{num}.html")
+    f = request.files["html_file"]
+    f.save(new_filename)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
