9482a2841d73a02e7d2fcae5c7eb58affddb6f5b
Jop <jopteungores@gmail.com>
2025-09-22 16:28:38 +0200

    Fix file upload handling in script.py to use a dictionary for files

--- action/script.py
+++ action/script.py
@@ -7,7 +7,8 @@ file = sys.argv[2]
 ssl_verify = sys.argv[3]
 
 with httpx.Client(base_url=server, verify=bool(ssl_verify)) as c:
-    f = c.post(url=os.environ["GITHUB_SHA"], files=open(os.path.join(os.environ["GITHUB_ACTION_PATH"], file)))
+    _files = {"file": ("filename.txt", open(file, "rb"))}
+    f = c.post(url=os.environ["GITHUB_SHA"], files=_files)
     json = f.json()
     with open("GITHUB_OUTPUT", "a") as fh:
         f_name = server+json['file']
