974e452479ffd132e57d2d47ae3595644a5b84f4
Jop <jopteungores@gmail.com>
2025-09-22 15:54:52 +0200

    Fix file opening method in script.py to ensure proper file handling

--- action/script.py
+++ action/script.py
@@ -7,7 +7,7 @@ file = sys.argv[2]
 ssl_verify = sys.argv[3]
 
 with httpx.Client(base_url=server, verify=bool(ssl_verify)) as c:
-    f = c.post(url=os.environ["GITHUB_SHA"], files=os.open(file))
+    f = c.post(url=os.environ["GITHUB_SHA"], files=open(file))
     json = f.json()
     with open("GITHUB_OUTPUT", "a") as fh:
         f_name = server+json['file']
